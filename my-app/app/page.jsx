273ae4d88f77fdddbc6a946eb417f3d5185440e7
KyanTG <kyan.gijtenbeek@hva.nl>
2025-10-27 15:33:58 +0100

    researched react / next.js

--- my-app/app/page.jsx
+++ my-app/app/page.jsx
@@ -1,10 +1,4 @@
-export default function Page() {
-  return <h1>Hello, Next.js!</h1>
-}
-
-import Image from 'next/image'
- 
-// export default function Page() {
-//   return <Image src="/public/file.svg" alt="Profile" width={100} height={100} />
-// }
+export default async function Page({ searchParams }) {
+    const filters = (await searchParams).filters
+  }
 
