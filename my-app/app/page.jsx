7cd1071d57f0f9ec1a92dcd8aa116a661dab6eea
KyanTG <kyan.gijtenbeek@hva.nl>
2025-10-29 19:10:10 +0100

    basics done and components shows

--- my-app/app/page.jsx
+++ my-app/app/page.jsx
@@ -1,3 +1,33 @@
-export default async function Page({ searchParams }) {
-  const filters = (await searchParams).filters
+import './App.css';
+import MyButton from "/components/components.jsx"
+
+function App() {
+  return (
+    <>
+      <section>
+        <h1>hi wereld</h1>
+      </section>
+      <Text />
+      <Custom show="kijk het werkt" />
+      <MyButton />
+    </>
+  );
 }
+
+function Text() {
+  return (
+    <>
+      <section>
+        <p>kiekeboe</p>
+      </section>
+    </>
+  )
+}
+
+function Custom({show}) {
+  return (
+    <p>{show}</p>
+  )
+}
+
+export default App
